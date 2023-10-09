a243ea07eedd7a9298c8aff31b0e702755f4a391
Danilus <Daniil_Merenkov12@mail.ru>
2023-10-09 20:04:48 +0300

    Initial commit

---
+++ src/main/java/org/example/Cats.java
@@ -0,0 +1,49 @@
+package org.example;
+public class Cats {
+    String id;
+    String text;
+    String type;
+    String user;
+    Integer upvotes;
+
+    public Cats(String id, String text, String type, String user, Integer upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public Cats() {
+    }
+
+    @Override
+    public String toString() {
+
+        return ("{ id = " + id +
+                "\ntext = \"" + text +
+                "\"\ntype = \"" + type + "\"\n"
+                + "user =  " + user + "\n"
+                + "upvotes = " + upvotes + " }");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+}
